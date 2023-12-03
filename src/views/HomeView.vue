1fb4d0e42c3af1b06379b5c93be911fa5c8044ac
Zolfikaar <fukar.work@gmail.com>
2023-12-03 23:10:04 +0300

    Complete basic functionality of suggestions page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,61 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
-
+import axios from "axios";
 import emptyIcon from '@/components/icons/empty.vue'
 import plusIcon from '@/components/icons/plus.vue' 
+import arrowUpIcon from '@/components/icons/arrowUp.vue'
+import commentsIcon from '@/components/icons/comments.vue'
 
 
 const router = useRouter()
 
 let feedbacks = ref([])
 
-const showModal = ref(false)
+const isVoted = ref(false)
 
 onMounted( async () => {
   getAllFeedback()
 })
 
 const getAllFeedback = async () => {
-    let response = await axios.get('@/data.json')
+  if(localStorage.getItem('feedbacks')){
+    feedbacks.value = JSON.parse(localStorage.getItem('feedbacks'))
+  } else {
 
+    let response = await axios.get('../../data.json')
     feedbacks.value = response.data.productRequests
+    // console.log(feedbacks.value[0].upvotes);
+    // console.log(feedbacks.value[0]);
+    saveFeedbacks(feedbacks.value)
+  }
 }
 
+const userVoteing = function (feedbackIndex) {
+  if (!feedbacks.value[feedbackIndex].isVoted) {
+    feedbacks.value[feedbackIndex].isVoted = true
+    feedbacks.value[feedbackIndex].upvotes++
+
+    // Retrieve existing data from local storage
+    const storedData = JSON.parse(localStorage.getItem('feedbacks')) || [];
+
+    // Update the specific feedback item in the local storage data
+    storedData[feedbackIndex] = feedbacks.value[feedbackIndex];
+
+    // Save the modified data back to local storage
+    localStorage.setItem('feedbacks', JSON.stringify(storedData));
+  }
+}
+
+// const updateFeedbackVotes = (feedback) => {
+//   let updatedItem = localStorage.getItem('feedbacks', JSON.parse(feedback));
+//   saveFeedbacks(feedbacks.value[updatedItem])
+//   // console.log(localStorage.getItem('feedbacks', JSON.parse(feedback)));
+// }
+
+const saveFeedbacks = function (feedback) {
+  localStorage.setItem('feedbacks',JSON.stringify(feedback))
+}
 </script>
 
 <template>
@@ -30,8 +64,59 @@ const getAllFeedback = async () => {
 
     <div class="content-box">
       
-      <div class="content" v-if="showModal">
-        content
+      <div class="suggestion-holder" v-if="feedbacks" >
+
+        <div class="suggestion" :class="{voted: feedback.isVoted}" v-for="(feedback, index) in feedbacks" :key="index">
+
+          <div class="left-side">
+
+            <div class="vote-count-box"  @click="userVoteing(index)">
+              <arrowUpIcon />
+              <span class="votes">{{ feedback.upvotes }}</span>
+            </div>
+
+            <div class="feedback-box">
+              <h3 class="feedback-header">{{ feedback.title }}</h3>
+              <p class="feedback-body">{{ feedback.description }}</p>
+              <div class="tags">
+                <span class="tag">{{ feedback.category }}</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="right-side comments">
+            <commentsIcon />
+            <span class="comment-count">{{ feedback.comments ? feedback.comments.length : 0 }}</span>
+          </div>
+          
+        </div>
+
+        <!-- <div class="suggestion" :class="{voted: isVoted}">
+
+          <div class="left-side">
+
+            <div class="vote-count-box">
+              <arrowUpIcon class="arrowUp" :style="{'stroke: #fff': isVoted }"/>
+              <span class="votes">66</span>
+            </div>
+
+            <div class="feedback-box">
+              <h3 class="feedback-header">Feedback Header</h3>
+              <p class="feedback-body">Feedback body, We love hearing about new ideas to improve our app.</p>
+              <div class="tags">
+                <span class="tag">UI</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="right-side comments">
+            <commentsIcon />
+            <span class="comment-count">4</span>
+          </div>
+          
+        </div> -->
       </div>
 
       <div class="no-feedback" v-else>
@@ -49,14 +134,87 @@ const getAllFeedback = async () => {
 </template>
 
 <style scoped>
-
 .content-box{
-  background-color: var(--white);
-  border-radius: 10px;
-  padding: 10px;
   margin-top: 20px;
 }
+.suggestion-holder .suggestion{
+  padding: 10px;
+  border-radius: 10px;
+  background-color: var(--white);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 150px;
+  margin-bottom: 20px;
+}
+.suggestion-holder .suggestion:hover{
+  cursor: pointer;
+}
+.suggestion-holder .suggestion .left-side{
+  display: flex;
+  margin-left: 20px;
+  width: 100%;
+}
+.suggestion-holder .suggestion .left-side .vote-count-box{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 53px;
+  border-radius: 10px;
+  background-color: var(--dark-gray);
+}
+.suggestion-holder .suggestion.voted .left-side .vote-count-box{
+  color: var(--white);
+  background-color: var(--dark-blue);
+}
+.suggestion-holder .suggestion .left-side .vote-count-box:hover{
+  background-color: #CFD7FF;
+  cursor: pointer;
+}
+.suggestion-holder .suggestion.voted .left-side .vote-count-box{
+  background-color: var(--dark-blue) ;
 
+}
+.suggestion-holder .suggestion .left-side .vote-count-box .votes{
+  margin-top: 8px;
+  font-weight: bold;
+}
+.suggestion-holder .suggestion .left-side .feedback-box{
+  margin: 0 20px 0 50px;
+}
+.suggestion-holder .suggestion .left-side .feedback-box:hover .feedback-header{
+  color: var(--dark-blue);
+  width: 100%;
+}
+.suggestion-holder .suggestion .left-side .feedback-box .feedback-body{
+  margin: 8px 0 20px 0;
+}
+.suggestion-holder .suggestion .left-side .feedback-box .tags {}
+.suggestion-holder .suggestion .left-side .feedback-box .tags .tag{
+  min-width: 48px;
+  text-align: center;
+  background-color: var(--dark-gray);
+  color: var(--dark-blue);
+  font-weight: bold;
+  font-size: 13px;
+  border-radius: 10px;
+  padding: 5px 15px;
+  margin: 5px;
+  margin-left: unset;
+}
+.suggestion-holder .suggestion .right-side{
+  margin-right: 20px;
+}
+.suggestion-holder .suggestion .right-side.comments{
+  display: flex;
+  align-items: center;
+}
+.suggestion-holder .suggestion .right-side .comment-count{
+  margin-left: 15px;
+  font-weight: bold;
+}
 .no-feedback{
   display: flex;
   flex-direction: column;
