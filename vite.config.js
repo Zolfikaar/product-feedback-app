c537b09b8a704a8489aa7c33a73efc43b6eaa6d4
Zolfikaar <fukar.work@gmail.com>
2023-12-30 04:41:43 +0300

    fixing vite.confix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/product-feedback-app/",
+  // base: "/product-feedback-app/",
   plugins: [vue()],
   resolve: {
     alias: {
